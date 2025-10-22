a1e8845581fe8bdc6402c05f0f580ccde880d6cf
Skidam <skidam4@gmail.com>
2025-10-22 14:58:19 +0100

    feat: add hello world and counter

--- anchor_counter_program/programs/anchor_counter_program/src/lib.rs
+++ anchor_counter_program/programs/anchor_counter_program/src/lib.rs
@@ -6,11 +6,29 @@ declare_id!("C7kmTo8Bxoq844esi4ppL4mLXoBG71LHvHG4UHvHWqv5");
 pub mod anchor_counter_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn hello(_ctx: Context<Hello>) -> Result<()> {
+
+        msg!("Hello World"); 
+
+        msg!("Our program's Program ID: {}", &id());
+
+        let mut counter = 0; 
+
+        let result = loop {
+            counter += 1; 
+            msg!("Current Count is: {counter}");
+            
+
+            if counter >= 10 {
+                break counter; 
+            }
+        };
+
+        msg!("The Final Count is {result}");
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Hello{}
